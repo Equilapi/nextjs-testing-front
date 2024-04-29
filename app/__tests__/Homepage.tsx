6bb2ab01dab6cd8a08b0765ebaa0842823e9d99f
Elvis Quilapi Castro <equilapi@meatcode.cl>
2024-04-29 16:02:43 -0400

    Home Page testing

---
+++ app/__tests__/Homepage.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import HomePage from '../page'
+
+describe("Home Page", () => {
+    it("renders home page", () => {
+        render(<HomePage/>)
+    
+        expect(screen.getByText(/Hello/i)).toBeInTheDocument()
+    });
+})
