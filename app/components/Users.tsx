0d2748412aae2bcdec44c0b91bcc5b8e6262c95b
Elvis Quilapi Castro <equilapi@meatcode.cl>
2024-04-29 17:17:57 -0400

    added flowbite components and footer, navbar custom

--- app/components/Users.tsx
+++ app/components/Users.tsx
@@ -1,14 +1,14 @@
-export default function Users({users}: any) {
+export default function Users({ users }: any) {
     return (
-        <ul>
+        <ul className="flex gap-5">
             {
-                users.map((user : any) => (
-                    <li key={user.id}>
-                        <div>
-                            <h5>{user.first_name} {user.last_name}</h5>
-                            <p>{user.email}</p>
-                        </div>
+                users.map((user: any) => (
+                    <li key={user.id} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <img src={user.avatar} alt={user.email} />
+                        <div>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{user.first_name} {user.last_name}</h5>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{user.email}</p>
+                        </div>
                     </li>
                 ))
             }
